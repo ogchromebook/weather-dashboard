1e559874d55e71fdd2748943a87b5ccd1248d1eb
Sam Kachergius <156481065+ogchromebook@users.noreply.github.com>
2024-07-25 19:02:21 -0500

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 // 696646672dc39fefaf604c403e03813a API KEY
 
-fetch (`https://api.openweathermap.org/data/2.5/weather?q=${"Chicago"}&appid=${"696646672dc39fefaf604c403e03813a"}`)
+
 .then (function (res){
     console.log(res)
     return res.json()
